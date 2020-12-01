10608461d4324388a635cf18c9db6dc8ab404847
sabbir-320 <sabbir195661@gmail.com>
2020-12-01 12:56:27 +0600

    project final

--- index.js
+++ index.js
@@ -11,20 +11,43 @@ app.use(bodyParser.json())
 
 
 // MongoDB Client
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ema-jhon.sxtxu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ema-jhon.sxtxu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("Ema-Jhon-Stor").collection("Product-Items");
-  // perform actions on the collection object
-    app.post('/addProduct', (req, res) => {
-        const products = req.body;
-        console.log(products);
-        collection.insertOne(products)
-        .then(result => {
-            console.log(result);
-        })
-    })
-  
+
+  // Post CRUD
+  app.post('/addProduct', (req, res) => {
+    const products = req.body;
+    collection.insertMany(products)
+      .then(result => {
+        res.send(result.insertedCount)
+      })
+  })
+
+  app.post('/productByKeys', (req, res) => {
+    const productKeys = req.body;
+    collection.find({ key: { $in: productKeys } })
+      .toArray((err, document) => {
+        res.send(document)
+      })
+  })
+
+  // Get CRUD
+  app.get('/procucts', (req, res) => {
+    collection.find({}).limit(20)
+      .toArray((err, document) => {
+        res.send(document)
+      })
+  })
+
+  app.get('/procucts/:key', (req, res) => {
+    collection.find({ key: req.params.key })
+      .toArray((err, document) => {
+        res.send(document[0])
+      })
+  })
+
 });
 
 
@@ -35,8 +58,8 @@ client.connect(err => {
 
 
 app.get('/', function (req, res) {
-    res.send('hello Rafiul')
-  })
+  res.send('hello Rafiul')
+})
 // PORT config
 app.listen(5000)
 console.log('5000 PORT successfully run');
